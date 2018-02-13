7f0a2c45c0248b856da74889108aacc794e93dc3
Bilal Günaydın <bilallgunaydin@gmail.com>
2018-02-13 15:40:27 +0300

    MVC - AJAX Örneği

---
+++ MVC - AJAX Örneği/AjaxOrnek/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxOrnek.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindEntities _dbContext;
+        public HomeController()
+        {
+            _dbContext = new NorthwindEntities();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetCategory(int id)
+        {
+            Category cat = _dbContext.Categories.FirstOrDefault(x => x.CategoryID == id);
+            return Json(cat, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MVC - AJAX Örneği/AjaxOrnek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxOrnek.MvcApplication" Language="C#" %>
